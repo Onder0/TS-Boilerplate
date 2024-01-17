a15e019a706226fc6a8513c4d1e983f5e1bb5c79
AmauryD <deflorenne.amaury@gmail.com>
2024-01-17 14:44:22 +0100

    fix plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
         es2021: true,
     },
     extends: ["prettier"],
-    plugins: ["prettier"],
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 async function init() {
     console.log("Project init");
-
-    if (true === true) {
-        console.log("aaa");
-    }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
